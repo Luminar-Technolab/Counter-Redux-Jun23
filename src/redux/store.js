ca83be98c28f263469cd2ac3051883663788fc3d
soniagkalliath <soniageorge111@gmail.com>
2023-10-16 12:30:25 +0530

    Counter with redux

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./counterSlice";
+
+export const store = configureStore({
+    reducer:{
+        //reducres should key-value pair
+        counter:counterSlice
+    }
+})
